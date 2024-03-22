91d342dfb4a1bc1c11ed8434bb200e10b4b541a6
AnatoliyL <anatoliynovgorodov6463@vk.com>
2024-03-22 21:23:54 +0300

    alpha-22032024.2123

--- Makefile
+++ Makefile
@@ -1,7 +1,5 @@
 .PHONY: all clean
 all:
-	cc nettrap.c -o nettrap -std=c11 -Wall -Wextra -Werror
-	cc addr/sell.c -o sell -std=c11 -Wall -Wextra -Werror
-
+	cc nettrap.c -o nettrap -std=c11 -Wall -Wextra
 clean:
-	rm nettrap sell
+	rm nettrap
--- nettrap.c
+++ nettrap.c
@@ -1,9 +1,12 @@
 #include <string.h>
+#include <unistd.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include "zio.h"
 #include "addr/shop.c"
-void bridge(int);
-void bridge(int money)
+#include "addr/sell.c"
+void bridge(int, char**);
+void bridge(int money, char **inventory)
 {
   printf("Area: Bridge\n");
   char adr[512];
@@ -13,33 +16,43 @@ void bridge(int money)
       if (strcmp(adr, "exit") == 0)
 	{
 	  printf("Thanks for playing nettrap! Hope you like it!\n");
-	  FILE *f = fopen("credits.txt", "r");
-	  int c;
-	  while ((c = fgetc(f)) != EOF)
-	    putchar(c);
 	  break;
 	}
       if (strcmp(adr, "shop") == 0)
-	shop(money);
-      if (strcmp(adr, "list") == 0)
+	shop(money, inventory);
+      else if (strcmp(adr, "sell") == 0)
+	{
+	  sell(money, inventory);
+	}
+      else if (strcmp(adr, "list") == 0)
 	  printf("shop, exit, list\n");
-      if (strcmp(adr, "exit") == 0)
-	break;
-
-      
       printf("Last input:%s\n", adr);
     }
 }
 
-int main()
+int main(void)
 {
-  FILE *f = fopen("startstory.txt", "r");
-  int c;
-  while ((c = fgetc(f)) != EOF)
-    {
-      putchar(c);
-    }
+  printf("\t\tStory\n\
+	 \n\n\
+	 It was a typical day, you opened browser,\watched some\n\
+	 videos, but then, everything dissapeared and you were sucked\n\
+	 into computer. You opened your eyes and saw, that you're in the WWW\n\
+        ");
+  sleep(7);
+  system("clear");
   int money = 75;
-  bridge(money);
+  char **inventory;
+  for (int i = 0; i < 64; i++)
+    {
+      for (int j = 0; j < 64; j++)
+	inventory = '\0';
+    }
+  printf("\
+      nettrap  Copyright (C) 2024  AnatoliyL\n\
+    This program comes with ABSOLUTELY NO WARRANTY; for details type `show w'.\n\
+    This is free software, and you are welcome to redistribute it\n\
+    under certain conditions; type `show c' for details.\n");
+
+  bridge(money, inventory);
   return 0;
 }
